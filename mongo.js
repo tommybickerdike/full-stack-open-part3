cf36664b90545e734fed14e6c5f51ddf627e2bf3
Tommy <tommy@musicglue.com>
2021-10-14 17:49:50 +0100

    3.12

---
+++ mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 5) {
+	console.log(
+		"Please provide the password, name and number as arguments: node mongo.js <password> <name> <number>"
+	);
+	process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://tommy:${password}@cluster0.4mqey.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+	name: String,
+	number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+	name: name,
+	number: number,
+});
+
+person.save().then((result) => {
+	console.log(`added ${name} number ${number} to phonebook`);
+	Person.find({}).then((result) => {
+		console.log("phonebook:");
+		result.forEach((person) => {
+			console.log(`${person.name} ${person.number}`);
+		});
+		mongoose.connection.close();
+	});
+});
